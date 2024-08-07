0d2b01d1f7fcdd70dc90f38fc81e1f9209eec57b
MarioM <a20300683@live.ceti.mx>
2024-08-06 23:59:06 -0600

    feat: :bug: agregando head en pestana (title & image)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,17 @@
 // pages/_app.js
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Tepepixqui</title>
+        <link rel="icon" href="/logo.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
