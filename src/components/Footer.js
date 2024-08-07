0924599a9d4c9c56c9879b5d349dd4f0d2a733f7
MarioM <a20300683@live.ceti.mx>
2024-08-06 23:39:06 -0600

    feat: :camera_flash: actualizacion de footer (se agrego opcion para descargar el apk de la app)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 // components/Footer.js
 
-import { FaInstagram } from 'react-icons/fa';
+
 import { AiFillAndroid, AiFillApple } from 'react-icons/ai';
 
 export default function Footer() {
@@ -13,17 +13,13 @@ export default function Footer() {
           <p className="text-sm">© 2024 Tepepixqui. Todos los derechos reservados.</p>
         </div>
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 items-center">
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300 transition-colors duration-200">
-            <FaInstagram size={24} />
-          </a>
-          <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300 transition-colors duration-200 flex items-center">
-            <AiFillAndroid size={24} className="mr-2" />
-            <span>Descargar para Android</span>
-          </a>
-          <a href="https://www.apple.com/ios/app-store/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300 transition-colors duration-200 flex items-center">
-            <AiFillApple size={24} className="mr-2" />
-            <span>Descargar para iOS</span>
-          </a>
+    
+            <a href="/tepepixqui.apk" download="tepepixqui.apk" className="text-white hover:text-gray-300 transition-colors duration-200 flex items-center">
+    <AiFillAndroid size={24} className="mr-2" />
+    <span>Descargar para Android</span>
+  </a>
+
+  
           <a href="mailto:tepepixqui.gdl@gmail.com" className="text-white hover:text-gray-300 transition-colors duration-200">
             <span>Enviar dudas: tepepixqui.gdl@gmail.com</span>
           </a>
